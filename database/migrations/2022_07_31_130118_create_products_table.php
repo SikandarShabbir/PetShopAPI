97c9cf22bc2885c3f58a9491f4a5afbda368a52d
Sikandar Shabbir <muhammadsikandar@pitb.gov.pk>
2022-07-31 18:46:05 +0500

    Adding products table migration.

---
+++ database/migrations/2022_07_31_130118_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(
+            'products',
+            function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->char('uuid', 36);
+                $table->foreignId('category_id')->nullable()->constrained()
+                    ->nullOnDelete();
+                $table->string('title');
+                $table->double('price', 12, 2);
+                $table->text('description');
+                $table->json('metadata');
+                $table->timestamps();
+                $table->softDeletes();
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
