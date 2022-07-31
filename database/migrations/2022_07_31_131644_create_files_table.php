6ae7deda40e28488835550ba1c83f45e7a8e637d
Sikandar Shabbir <muhammadsikandar@pitb.gov.pk>
2022-07-31 18:46:25 +0500

    Adding files table migration.

---
+++ database/migrations/2022_07_31_131644_create_files_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(
+            'files',
+            function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->char('uuid', 36);
+                $table->string('name');
+                $table->string('path');
+                $table->string('size');
+                $table->string('type');
+                $table->timestamps();
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+};
