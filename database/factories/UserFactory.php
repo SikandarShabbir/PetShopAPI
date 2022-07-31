f9e944d79406f46c17160598aed49dc5b6c36719
Sikandar Shabbir <muhammadsikandar@pitb.gov.pk>
2022-07-31 19:23:48 +0500

    Adding Use, Product, and Category Factories.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -18,11 +19,17 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->safeEmail(),
+            'uuid' => fake()->uuid,
+            'first_name' => fake()->name(),
+            'last_name' => fake()->name(),
+            'is_admin' => rand(0,1),
+            'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'password' => Hash::make('password'),
+            'avatar' => fake()->uuid,
+            'address' => fake()->address,
+            'phone_number' => fake()->phoneNumber,
+            'is_marketing' => rand(0,1)
         ];
     }
 
