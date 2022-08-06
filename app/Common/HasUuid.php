85ee9ae204abe78cd9d34ac7a1e506525efa6bb3
Sikandar Shabbir <muhammadsikandar@pitb.gov.pk>
2022-08-07 00:19:17 +0500

    Adding Uuid trait on User Model.

---
+++ app/Common/HasUuid.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Common;
+
+use Illuminate\Support\Str;
+
+trait HasUuid
+{
+    public static function bootHasUUID(): void
+    {
+        static::creating(
+            function ($model) {
+                $model->uuid = Str::uuid()->toString();
+            }
+        );
+    }
+}
