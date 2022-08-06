06885b6a564d4940ced75462920ba8e5edd9736c
Sikandar Shabbir <muhammadsikandar@pitb.gov.pk>
2022-08-07 02:11:59 +0500

    Adding admin and user accounts to User seeder.

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -14,11 +14,15 @@ class EventServiceProvider extends ServiceProvider
      *
      * @var array<class-string, array<int, class-string>>
      */
-    protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
-    ];
+    protected $listen
+        = [
+            Registered::class                      => [
+                SendEmailVerificationNotification::class,
+            ],
+            'Illuminate\Auth\Events\Authenticated' => [
+                'App\Listeners\LogSuccessfulLogin',
+            ],
+        ];
 
     /**
      * Register any events for your application.
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -15,6 +16,37 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        User::create(
+            [
+                'uuid'              => fake()->uuid(),
+                'first_name'        => 'Admin',
+                'last_name'         => 'admin',
+                'is_admin'          => 1,
+                'email'             => 'admin@gmail.com',
+                'email_verified_at' => now(),
+                'password'          => 'admin',
+                'avatar'            => fake()->uuid(),
+                'address'           => fake()->address(),
+                'phone_number'      => fake()->phoneNumber(),
+                'is_marketing'      => fake()->numberBetween(0, 1)
+            ]
+        );
+        User::create(
+            [
+                'uuid'              => fake()->uuid(),
+                'first_name'        => 'User',
+                'last_name'         => 'user',
+                'is_admin'          => 0,
+                'email'             => 'user@gmail.com',
+                'email_verified_at' => now(),
+                'password'          => 'user',
+                'avatar'            => fake()->uuid(),
+                'address'           => fake()->address(),
+                'phone_number'      => fake()->phoneNumber(),
+                'is_marketing'      => fake()->numberBetween(0, 1)
+            ]
+        );
+
         User::factory(10)->create();
     }
 }
