9a616b5929271175a54ffa642f17deaa3b5ec111
Sikandar Shabbir <muhammadsikandar@pitb.gov.pk>
2022-08-07 09:43:01 +0500

    Adding admin and user accounts to User seeder.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,6 +15,37 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        User::create(
+            [
+                'uuid'              => fake()->uuid(),
+                'first_name'        => 'Admin',
+                'last_name'         => 'admin',
+                'is_admin'          => 1,
+                'email'             => 'admin@gmail.com',
+                'email_verified_at' => now(),
+                'password'          => 'admin',
+                'avatar'            => fake()->uuid(),
+                'address'           => fake()->address(),
+                'phone_number'      => fake()->phoneNumber(),
+                'is_marketing'      => fake()->numberBetween(0, 1)
+            ]
+        );
+        User::create(
+            [
+                'uuid'              => fake()->uuid(),
+                'first_name'        => 'User',
+                'last_name'         => 'user',
+                'is_admin'          => 0,
+                'email'             => 'user@gmail.com',
+                'email_verified_at' => now(),
+                'password'          => 'user',
+                'avatar'            => fake()->uuid(),
+                'address'           => fake()->address(),
+                'phone_number'      => fake()->phoneNumber(),
+                'is_marketing'      => fake()->numberBetween(0, 1)
+            ]
+        );
+        
         User::factory(10)->create();
     }
 }
