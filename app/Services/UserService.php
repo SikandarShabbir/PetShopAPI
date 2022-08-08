d7795b72c51f1d5493a02fe2a47318b9817bf940
Sikandar Shabbir <muhammadsikandar@pitb.gov.pk>
2022-08-09 00:27:01 +0500

    Adding user service class with business logic for user controller.

---
+++ app/Services/UserService.php
@@ -0,0 +1,87 @@
+<?php
+
+
+namespace App\Services;
+
+use App\Models\User;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use phpDocumentor\Reflection\Types\Never_;
+
+class UserService
+{
+    public User $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @param  string  $uuid
+     *
+     * @return void|never
+     */
+    public function validateAdmin(string $uuid)
+    {
+        if ($this->user->where('uuid', $uuid)->value('is_admin') === 'admin') {
+            $this->throwHttpResponseException(
+                'Unauthorized: Not enough privileges'
+            );
+        }
+    }
+
+    /**
+     * @param  string  $uuid
+     *
+     * @return void|never
+     */
+    public function checkUserExistence(string $uuid)
+    {
+        if ($this->user->where('uuid', $uuid)->doesntExist()) {
+            $this->throwHttpResponseException(
+                'User not found'
+            );
+        }
+    }
+
+    /**
+     * @param  Request  $request
+     *
+     * @return array|void
+     */
+    public function authenticateUser(Request $request)
+    {
+        $user = $this->user->where('email', '=', $request->email)->first();
+        if ($user) {
+            if (Hash::check($request->password, $user->password)) {
+                return [
+                    'token' => $this->user->refreshToken($request)
+                ];
+            }
+        }
+        $this->throwHttpResponseException(
+            'Failed to authenticate user'
+        );
+    }
+
+    public function throwHttpResponseException(
+        string $error
+    ): never {
+        throw new HttpResponseException(
+            response()->json(
+                [
+                    "success" => 0,
+                    "data"    => [],
+                    "error"   => $error,
+                    "errors"  => [],
+                    "trace"   => []
+                ],
+                Response::HTTP_UNPROCESSABLE_ENTITY
+            )
+        );
+    }
+}
