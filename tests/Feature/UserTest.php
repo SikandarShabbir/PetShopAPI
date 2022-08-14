d5d95c51629e2bd1221ca2dc0bb340bed70d8499
Sikandar Shabbir <muhammadsikandar@pitb.gov.pk>
2022-08-15 01:04:15 +0500

    Get user details API endpoint feature tests.

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -223,5 +223,207 @@ class UserTest extends TestCase
             ]
         )->assertStatus(200);
     }
+
+    //    Get User details
+
+    public function test_getting_user_accounts_listing_without_authorization_is_unsuccessful(
+    )
+    {
+        $response = $this->get(
+            '/api/v1/admin/user-listing'
+        );
+
+        $response->assertJson(['error' => 'Unauthorized'])
+            ->assertStatus(401);
+    }
+
+    public function test_listing_user_accounts_as_authorized_Admin_user()
+    {
+        $response = $this->post(
+            '/api/v1/admin/login',
+            [
+                'email'    => 'admin@gmail.com',
+                'password' => 'admin'
+            ]
+        );
+
+        $token = $response->getOriginalContent()['data']['token'];
+        $response = $this->getJson(
+            '/api/v1/admin/user-listing?limit=1',
+            [
+                'Authorization' => 'Bearer '.$token,
+                'Accept'        => 'application/json'
+            ]
+        );
+        $response->assertJsonStructure(
+            [
+                'current_page',
+                'data' => [
+                    '*' => [
+                        'uuid',
+                        'first_name',
+                        'last_name',
+                        'email',
+                        'email_verified_at',
+                        'avatar',
+                        'address',
+                        'phone_number',
+                        'is_marketing',
+                        'created_at',
+                        'updated_at',
+                        'last_login_at',
+                    ]
+                ],
+                'first_page_url',
+                'from',
+                'last_page',
+                'last_page_url',
+                'links',
+                'last_page_url',
+                'next_page_url',
+                'path',
+                'per_page',
+                'prev_page_url',
+                'to',
+                'total',
+            ]
+        )->assertStatus(
+            200
+        );
+    }
+
+    //    Admin Update Endpoint
+    public function test_Checking_admin_update_endpoint_as_unauthorized_user()
+    {
+        $response = $this->put(
+            '/api/v1/admin/user-edit/{uuid}'
+        );
+
+        $response->assertJson(
+            [
+                'success' => 0,
+                'data'    => [],
+                'error'   => 'Unauthorized',
+                'errors'  => [],
+                'trace'   => []
+            ]
+        )->assertStatus(
+            401
+        );
+    }
+
+    public function test_checking_validations_as_authorized_user_for_admin_account_update_endpoint(
+    )
+    {
+        $response = $this->post(
+            '/api/v1/admin/login',
+            [
+                'email'    => 'admin@gmail.com',
+                'password' => 'admin'
+            ]
+        );
+
+        $token = $response->getOriginalContent()['data']['token'];
+
+        $response = $this->putJson(
+            '/api/v1/admin/user-edit/{uuid}',
+            [],
+            [
+                'Authorization' => 'Bearer '.$token,
+                'Accept'        => 'application/json'
+            ]
+        );
+        $response->assertJson(
+            [
+                'success' => 0,
+                'data'    => [],
+                'error'   => 'Failed Validation',
+                'errors'  => [
+                    'first_name'            => [
+                        'The first name field is required.'
+                    ],
+                    'last_name'             => [
+                        'The last name field is required.'
+                    ],
+                    'email'                 => [
+                        'The email field is required.'
+                    ],
+                    'password'              => [
+                        'The password field is required.'
+                    ],
+                    'password_confirmation' => [
+                        'The password confirmation field is required.'
+                    ],
+                    'address'               => [
+                        'The address field is required.'
+                    ],
+                    'phone_number'          => [
+                        'The phone number field is required.'
+                    ]
+                ],
+                'trace'   => []
+
+            ]
+        )->assertStatus(
+            422
+        );
+    }
+
+    public function test_trying_to_update_user_account_as_authorized_admin()
+    {
+        $uuid = User::where('is_admin', 0)->value('uuid');
+        $response = $this->post(
+            '/api/v1/admin/login',
+            [
+                'email'    => 'admin@gmail.com',
+                'password' => 'admin'
+            ]
+        );
+
+        $token = $response->getOriginalContent()['data']['token'];
+
+        $response = $this->putJson(
+            '/api/v1/admin/user-edit/'.$uuid,
+            [
+                'first_name'            => 'ABCD',
+                'last_name'             => 'ABCD',
+                'email'                 => 'user@gmail.com',
+                'password'              => 'password',
+                'password_confirmation' => 'password',
+                'avatar'                => 'ABCD',
+                'address'               => 'ABCD',
+                'phone_number'          => '0987654321',
+                'marketing'             => 'marketing',
+            ],
+            [
+                'Authorization' => 'Bearer '.$token,
+                'Accept'        => 'application/json'
+            ]
+        );
+
+        $response->assertJsonStructure(
+            [
+                'success',
+                'data' => [
+                    '*' => [
+                        'uuid',
+                        'first_name',
+                        'last_name',
+                        'email',
+                        'email_verified_at',
+                        'address',
+                        'phone_number',
+                        'is_marketing',
+                        'created_at',
+                        'updated_at',
+                        'last_login_at',
+                    ]
+                ],
+                'error',
+                'errors',
+                'extra',
+            ]
+        )->assertStatus(200);
+    }
     
 }
