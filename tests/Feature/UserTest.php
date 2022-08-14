0b69d83f71aef7a963d8c038a0c224697486ef3c
Sikandar Shabbir <muhammadsikandar@pitb.gov.pk>
2022-08-15 01:02:45 +0500

    Admin create API Endpoint feature test.

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -121,4 +121,107 @@ class UserTest extends TestCase
         )->assertStatus(200);
     }
 
+    //    Admin Create Account API Endpoint
+    public function test_Checking_if_the_admin_create_user_api_validations_working_fine(
+    )
+    {
+        $response = $this->post(
+            '/api/v1/admin/create'
+        );
+
+        $response->assertJson(
+            [
+                'success' => 0,
+                'data'    => [],
+                'error'   => 'Failed Validation',
+                'errors'  => [
+                    'first_name'            => [
+                        'The first name field is required.'
+                    ],
+                    'last_name'             => [
+                        'The last name field is required.'
+                    ],
+                    'email'                 => [
+                        'The email field is required.'
+                    ],
+                    'password'              => [
+                        'The password field is required.'
+                    ],
+                    'password_confirmation' => [
+                        'The password confirmation field is required.'
+                    ],
+                    'avatar'                => [
+                        'The avatar field is required.'
+                    ],
+                    'address'               => [
+                        'The address field is required.'
+                    ],
+                    'phone_number'          => [
+                        'The phone number field is required.'
+                    ]
+                ],
+                'trace'   => []
+            ]
+        )->assertStatus(422);
+    }
+
+    public function test_generating_invalid_email_and_mismatch_token_validation_error_on_admin_create_api_endpoint(
+    )
+    {
+        $response = $this->post(
+            '/api/v1/admin/create',
+            [
+                'email'                 => 'abcd',
+                'password'              => 'password',
+                'password_confirmation' => 'passwor'
+            ]
+        );
+
+        $response->assertJson(
+            [
+                'success' => 0,
+                'data'    => [],
+                'error'   => 'Failed Validation',
+                'errors'  => [
+                    'email'                 => [
+                        'The email must be a valid email address.'
+                    ],
+                    'password_confirmation' => [
+                        'The password confirmation and password must match.'
+                    ]
+                ],
+                'trace'   => []
+            ]
+        )->assertStatus(422);
+    }
+
+    public function test_creating_a_Admin_account_with_all_information_provided(
+    )
+    {
+        $response = $this->post(
+            '/api/v1/admin/create',
+            [
+                'first_name'            => 'updated first name',
+                'last_name'             => 'updated last name',
+                'email'                 => fake()->unique()->safeEmail(),
+                'password'              => 'password',
+                'password_confirmation' => 'password',
+                'avatar'                => fake()->uuid(),
+                'address'               => fake()->address(),
+                'phone_number'          => fake()->phoneNumber(),
+                'marketing'             => 'marketing',
+            ]
+        );
+
+        $response->assertJson(
+            [
+                'success' => 1,
+                'data'    => [],
+                'error'   => '',
+                'errors'  => [],
+                'extra'   => []
+            ]
+        )->assertStatus(200);
+    }
+    
 }
